5be252bb7a9a6464a38e13b01192c3a575389605
Victoria B <viktorija.bourassa@gmail.com>
2019-03-01 16:13:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,13 @@ module FormsLab
     end
 
     post "/pirates" do
-    erb :show
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
+    erb :'pirates/show'
     end
 
   end
